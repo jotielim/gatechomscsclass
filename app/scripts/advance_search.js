da69c7c6566b948b09700606777b03e1dd25a9e9
Josh Chao <Josh.Chao@careerbuilder.com>
2015-04-02 20:08:57 -0400

    Updated term to summer 2015 and added CSE as subject

--- app/scripts/advance_search.js
+++ app/scripts/advance_search.js
@@ -8,7 +8,16 @@ var subject = document.querySelector('#subj_id');
 var campus = document.querySelector('#camp_id');
 var advanceSearchBtn = document.querySelector('form[action="/pls/bprod/bwskfcls.P_GetCrse_Advanced"] input[type="submit"]');
 
-subject.value = 'CS';
+var target_subjects = ['CS','CSE'];
+
+for(var i = 0; i < subject.length; i++) {
+    for (var k = 0; k < target_subjects.length; k++){
+        if(subject.options[i].value == target_subjects[k]) {
+            subject.options[i].selected = true;
+        }
+    }
+}
+
 campus.value = 'O';
 
 advanceSearchBtn.click();
--- app/scripts/select_term.js
+++ app/scripts/select_term.js
@@ -11,11 +11,13 @@ var currentTerm;
 
 for (var i = 0; i < options.length; i++) {
   currentTerm = options[i];
-  if (currentTerm.value && currentTerm.value.length > 0) {
+  if (currentTerm.value == '201505' && currentTerm.value.length > 0) {
     break;
   }
   currentTerm = null;
 }
+
+// '201508' for Fall 2015
 
 if (select && currentTerm) {
   select.value = currentTerm.value;
