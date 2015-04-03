23c6337b9169b1df02b9e9c6bbb38f4bad2abd33
Josh Chao <Josh.Chao@careerbuilder.com>
2015-04-02 20:17:02 -0400

    Use proper naming convention

--- app/scripts/advance_search.js
+++ app/scripts/advance_search.js
@@ -8,16 +8,15 @@ var subject = document.querySelector('#subj_id');
 var campus = document.querySelector('#camp_id');
 var advanceSearchBtn = document.querySelector('form[action="/pls/bprod/bwskfcls.P_GetCrse_Advanced"] input[type="submit"]');
 
-var target_subjects = ['CS','CSE'];
+var subjectsToSelect = ['CS','CSE'];
 
 for(var i = 0; i < subject.length; i++) {
-    for (var k = 0; k < target_subjects.length; k++){
-        if(subject.options[i].value == target_subjects[k]) {
-            subject.options[i].selected = true;
-        }
+  for (var k = 0; k < subjectsToSelect.length; k++){
+    if(subject.options[i].value == subjectsToSelect[k]) {
+      subject.options[i].selected = true;
     }
+  }
 }
-
 campus.value = 'O';
 
 advanceSearchBtn.click();
--- app/scripts/select_term.js
+++ app/scripts/select_term.js
@@ -11,13 +11,11 @@ var currentTerm;
 
 for (var i = 0; i < options.length; i++) {
   currentTerm = options[i];
-  if (currentTerm.value == '201505' && currentTerm.value.length > 0) {
+  if (currentTerm.value == '201505' /*'201508' for Fall 2015*/ && currentTerm.value.length > 0) {
     break;
   }
   currentTerm = null;
 }
-
-// '201508' for Fall 2015
 
 if (select && currentTerm) {
   select.value = currentTerm.value;
